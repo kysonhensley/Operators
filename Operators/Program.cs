a221654db892d7fffe1fe1517514ea8da3eea7a2
kysonhensley <hensleykyson@gmail.com>
2026-01-20 08:16:46 -0700

    add simple arithmetic operation

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine(5 + 5); // addition
+            Console.WriteLine(5 - 5); // subtraction
+            Console.WriteLine(5 * 5); // multiplication
+            Console.WriteLine(13 / 5); // integer division
+            Console.WriteLine(13 % 5); // modulus operator
+            Console.WriteLine(13d / 5d); // floating point division
+            Console.WriteLine(13.0 / 5); // floating point division
+            Console.WriteLine(13 / 5.0); // floating point division
+            Console.WriteLine(13.0 / 5.0); // floating point division
+
+            //pause
+            Console.Read();
         }
     }
 } 
